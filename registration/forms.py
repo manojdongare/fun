2f096f9a1f8ed714b7baf0e75fc26516b9b8154e
manojdongare <manojdongare@gmail.com>
2020-04-04 21:11:58 +0530

    my pro

---
+++ registration/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+#DataFlair #Form
+class SignUp(forms.Form):
+    first_name = forms.CharField(initial = 'First Name', )
+    last_name = forms.CharField()
+    email = forms.EmailField(help_text = 'write your email', )
+    Address = forms.CharField(required = False, )
+    Technology = forms.CharField(initial = 'Django', disabled = True, )
+    age = forms.IntegerField()
+    password = forms.CharField(widget = forms.PasswordInput)
+    re_password = forms.CharField(help_text = 'renter your password', widget = forms.PasswordInput)
